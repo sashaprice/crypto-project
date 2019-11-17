ffa7c272dee81a85b9cffedf33b72b115c281b67
sashaprice <iprice@unomaha.edu>
2019-11-17 14:36:09 -0600

    adding file checking and images used for testing and examples

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,6 +1,17 @@
 class Test {
     static void main(String[] args) {
         // Run this to test if your configuration works properly
-        println('Main method has run successfully')
+        String imagePath = getBasePath() + "\\data\\input\\building-1.jpg"
+        try {
+            File file = new File(imagePath) // Verifies image is found
+            println("File was successfully found")
+        }
+        catch (FileNotFoundException ignored) {
+            println("Path-finding is broken")
+        }
+    }
+
+    private static String getBasePath() {
+        return new File("").getAbsolutePath()
     }
 }
