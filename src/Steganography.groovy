6435cb3f9b21e0951de40e8b73766e06cde20a19
sashaprice <iprice@unomaha.edu>
2019-11-26 13:09:05 -0600

    replacing original LSB with RLSB, as original is inefficient and not secure

--- src/Steganography.groovy
+++ src/Steganography.groovy
@@ -17,69 +17,7 @@ class Steganography {
     }
 
     /**
-     * Encrypts an image with given data as an array of bytes. Encryption is performed by
-     * replacing the first 2 bits of each RGB value with 2 bits from the data sequentially.
-     * Bits are encoded left to right from the data into the RGB. Pixel RGB values also
-     * contain an channel, but it is not used in LSB.
-     *
-     * An RGB value is 4 bytes, formatted as such:
-     *         1        2    3    4
-     * [ALPHA CHANNEL][ R ][ G ][ B ]
-     *
-     * @param image the base image to encipher the data
-     * @param data the data to encipher into the image
-     * @return an image enciphered with the data
-     */
-    private static BufferedImage encryptLSB(BufferedImage image, byte[] data) {
-        int height = image.height
-        int width = image.width
-        assert height * width >= (4 * data.length / 3)
-        BufferedImage encryptedImage = deepCopy(image)
-        for (int i = 0; i < data.length * 4; ++i) {
-            int x = (i.intdiv(3)) % width
-            int y = i.intdiv(3 * width)
-            int RGB = encryptedImage.getRGB(x, y)
-
-            // Takes a pair of bits from a byte in the data and shifts them to the right end
-            byte pair = (byte) ((data[i.intdiv(4)] & 0b11 << 6 - 2 * (i % 4)) >> 2 * (3 - (i % 4)))
-
-            // Creates a 2-bit-long hole in the RGB value and fills it with the pair of bits
-            int encodedRGB = RGB & ~(0b11 << 8 * (2 - (i % 3))) | pair << (8 * (2 - (i % 3)))
-            encryptedImage.setRGB(x, y, encodedRGB)
-        }
-        return encryptedImage
-    }
-
-    /**
-     * Decodes a number of bytes equal to the given length from an image. The bytes are composed
-     * of the left-most pairs of bits taken from each R, G, and B value in the image's pixels
-     * starting from the pixel at 0, 0 and moving left to right and top to bottom.
-     *
-     * @param image the image containing the data to decode
-     * @param length the number of bytes to pull from the image
-     * @return the decoded data as an array of bytes
-     */
-    private static byte[] decryptLSB(BufferedImage image, long length) {
-        int height = image.height
-        int width = image.width
-        assert height * width >= (4 * length / 3)
-        byte[] data = new byte[length]
-        for (int i = 0; i < length * 4; ++i) {
-            int x = (i.intdiv(3)) % width
-            int y = i.intdiv(3 * width)
-            int RGB = image.getRGB(x, y)
-
-            // Gets the pair of bits from the RGB value
-            byte pair = (byte) (0b11 & RGB >> 8 * (2 - (i % 3)))
-
-            // Combines the pair shifted to the correct position with the current byte
-            data[i.intdiv(4)] |= (byte) (pair << 2 * (3 - (i % 4)))
-        }
-        return data
-    }
-
-    /**
-     * Encodes an image using the LSB algorithm (see {@link Steganography#encryptLSB}). The
+     * Encodes an image using the LSB algorithm. The
      * indeces are selected from a pseudo-random number generator so that the encoded bits are
      * distributed randomly throughout the pixels. However, the continuity is guaranteed by
      * using the same seed for the generator.
@@ -89,7 +27,7 @@ class Steganography {
      * @param seed the seed of the PRNG
      * @return an image encoded with the data
      */
-    static BufferedImage encryptRLSB(BufferedImage image, byte[] data, long seed) {
+    static BufferedImage encodeLSB(BufferedImage image, byte[] data, long seed) {
         int height = image.height
         int width = image.width
         assert height * width >= (4 * data.length / 3)
@@ -117,7 +55,7 @@ class Steganography {
     }
 
     /**
-     * Decodes bits in the image via the LSB algorithm (see {@link Steganography#encryptLSB}). Pixels
+     * Decodes bits in the image via the LSB algorithm. Pixels
      * are randomly selected based on a given seed, and the pairs of bits are accumulated into a number
      * of bytes equal to the given length.
      *
@@ -126,7 +64,7 @@ class Steganography {
      * @param seed the seed of the PRNG
      * @return the decoded data
      */
-    static byte[] decryptRLSB(BufferedImage image, long length, long seed) {
+    static byte[] decodeLSB(BufferedImage image, long length, long seed) {
         int height = image.height
         int width = image.width
         assert height * width >= (4 * length / 3)
