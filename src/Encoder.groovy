41df9e2032fcd93103b1be61f164c17bd63fa651
sashaprice <57649389+sashaprice@users.noreply.github.com>
2019-11-27 14:36:36 -0600

    Create Encoder.groovy

---
+++ src/Encoder.groovy
@@ -0,0 +1,75 @@
+class Encoder {
+    /**
+     * Encodes an image using the LSB algorithm. The
+     * indeces are selected from a pseudo-random number generator so that the encoded bits are
+     * distributed randomly throughout the pixels. However, the continuity is guaranteed by
+     * using the same seed for the generator.
+     *
+     * @param image the image to encode the data into
+     * @param data the data to encode
+     * @param seed the seed of the PRNG
+     * @return an image encoded with the data
+     */
+    static BufferedImage encodeLSB(BufferedImage image, byte[] data, long seed) {
+        int height = image.height
+        int width = image.width
+        assert height * width >= (4 * data.length / 3)
+        BufferedImage encryptedImage = deepCopy(image)
+
+        // SecureRandom is used to keep the selection pattern undetectable
+        SecureRandom random = new SecureRandom(toBytes(seed))
+
+        // Used arrays keep track of pixels that have already been encoded for each color (i.e. R, G, and B)
+        List<Integer>[] usedX = new List<Integer>[] {new ArrayList<>(0..width - 1), new ArrayList<>(0..width - 1), new ArrayList<>(0..width - 1)}
+        List<Integer>[] usedY = new List<Integer>[] {new ArrayList<>(0..height - 1), new ArrayList<>(0..height - 1), new ArrayList<>(0..height - 1)}
+        for (int i = 0; i < data.length * 4; ++i) {
+            int x = usedX[i % 3].remove(random.nextInt(usedX[i % 3].size()))
+            int y = usedY[i % 3].remove(random.nextInt(usedY[i % 3].size()))
+            int RGB = encryptedImage.getRGB(x, y)
+
+            // Takes a pair of bits from a byte in the data and shifts them to the right end
+            byte pair = (byte) ((data[i.intdiv(4)] & 0b11 << 6 - 2 * (i % 4)) >> 2 * (3 - (i % 4)))
+
+            // Creates a 2-bit-long hole in the RGB value and fills it with the pair of bits
+            int encodedRGB = RGB & ~(0b11 << 8 * (2 - (i % 3))) | pair << (8 * (2 - (i % 3)))
+            encryptedImage.setRGB(x, y, encodedRGB)
+        }
+        return encryptedImage
+    }
+    
+    /**
+     * Decodes bits in the image via the LSB algorithm. Pixels
+     * are randomly selected based on a given seed, and the pairs of bits are accumulated into a number
+     * of bytes equal to the given length.
+     *
+     * @param image the image to decode the data from
+     * @param length the number of bytes to decode
+     * @param seed the seed of the PRNG
+     * @return the decoded data
+     */
+    static byte[] decodeLSB(BufferedImage image, long length, long seed) {
+        int height = image.height
+        int width = image.width
+        assert height * width >= (4 * length / 3)
+        byte[] data = new byte[length]
+
+        // SecureRandom is used to keep the selection pattern undetectable
+        SecureRandom random = new SecureRandom(toBytes(seed))
+
+        // Used arrays keep track of pixels that have already been encoded for each color (i.e. R, G, and B)
+        List<Integer>[] usedX = new List<Integer>[] {new ArrayList<>(0..width - 1), new ArrayList<>(0..width - 1), new ArrayList<>(0..width - 1)}
+        List<Integer>[] usedY = new List<Integer>[] {new ArrayList<>(0..height - 1), new ArrayList<>(0..height - 1), new ArrayList<>(0..height - 1)}
+        for (int i = 0; i < length * 4; ++i) {
+            int x = usedX[i % 3].remove(random.nextInt(usedX[i % 3].size()))
+            int y = usedY[i % 3].remove(random.nextInt(usedY[i % 3].size()))
+            int RGB = image.getRGB(x, y)
+
+            // Gets the pair of bytes from one of the color channels and shifts it to the right
+            byte pair = (byte) (0b11 & RGB >> 8 * (2 - (i % 3)))
+
+            // Places the pair of bytes in the correct position in the data
+            data[i.intdiv(4)] |= (byte) (pair << 2 * (3 - (i % 4)))
+        }
+        return data
+    }
+}
